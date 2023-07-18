4e659a594bcdb158528fc13099d2f68df1b2586d
Naseem Khan <nk8257949@gmail.com>
2023-07-18 00:52:29 -0700

    production error fixes

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -4,19 +4,19 @@ import { useEffect, useRef } from "react";
 import { BsArrowUp } from "react-icons/bs";
 
 const ScrollToTop = () => {
-  const arrow = useRef();
+  const arrow = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     window.onscroll = () => {
       if (window.pageYOffset >= 180) {
-        arrow.current.classList.add("right-10");
+        arrow.current?.classList.add("right-10");
       } else {
-        arrow.current.classList.remove("right-10");
+        arrow.current?.classList.remove("right-10");
       }
     };
-  }, []);
+  });
 
-  const scrollToTop = () => {
+  const scrollTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
@@ -27,9 +27,9 @@ const ScrollToTop = () => {
     <div className="overflow-hidden">
       <button
         type="button"
-        className="commonBtn text-xs fixed bottom-6 ss:bottom-10 z-30 -right-full transition-all duration-500 shadow-2xl active:p-[.45rem] p-2 rounded"
-        onClick={scrollToTop}
         ref={arrow}
+        onClick={scrollTop}
+        className="commonBtn text-xs fixed bottom-6 ss:bottom-10 z-30 -right-full transition-all duration-500 shadow-2xl active:p-[.45rem] p-2 rounded"
       >
         <span className="text">
           <BsArrowUp className="text-xl active:text-lg text-white" />
